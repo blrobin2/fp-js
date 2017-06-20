a58d72aca4dd2f5ae764b54f780d0a36de6d9e12
Bennie <brobinson88@gmail.com>
2017-06-20 09:26:42 -0400

    Combine Future and IO example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { curry }     from 'ramda'
-import { Maybe, IO } from 'ramda-fantasy'
+import { curry, prop }     from 'ramda'
+import { Maybe, IO, Future } from 'ramda-fantasy'
 const Just    = Maybe.Just
 const Nothing = Maybe.Nothing
 
@@ -9,7 +9,31 @@ const safeDiv = curry((n: number, d: number) =>
     ? Nothing()
     : Just(n / d))
 
-//stdoutWrite :: String -> IO()
-const stdoutWrite = (data: string) => IO(() => console.log(data))
+//githubUser :: String -> String
+const githubUser = (username: string) =>
+  `https://api.github.com/users/${username}`
 
-stdoutWrite(safeDiv(3,2)).runIO()
+//getNumRepos :: String -> Future Number
+const getNumRepos = (username: string) =>
+  httpRequest(githubUser(username)).map(prop('public_repos'))
+
+// UNSAFE STUFF WITH SIDE-EFFECTS
+
+//httpRequest :: String -> Future JSON
+const httpRequest = (url: string) =>
+  Future((reject, resolve) =>
+    fetch(url)
+      .then(response =>
+        response.json())
+      .then(resolve)
+      .catch(reject))
+
+//stdoutWrite :: String -> IO ()
+const stdoutWrite = (data: string) =>
+  IO(() => console.log(data))
+
+//unnecessaryDivide :: Number -> IO ()
+const unnecessaryDivide = (num: number) =>
+  stdoutWrite(safeDiv(num)(2).toString()).runIO()
+
+getNumRepos('blrobin2').fork(console.error, unnecessaryDivide)
